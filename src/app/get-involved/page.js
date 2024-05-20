20630b0a8a412337e89976786b9982d1b66add9b
JavaG23 <geof.hoffman@gmail.com>
2024-05-20 15:05:20 -0400

    updated eslint rules

--- src/app/get-involved/page.js
+++ src/app/get-involved/page.js
@@ -2,7 +2,7 @@ export default function Page() {
     return <a
     href="/"  
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:dark:bg-neutral-800/30"
-          target=""
+          
           rel="noopener noreferrer"><p>"Get Involved" Page- coming soon. Email me at geof.hoffman@gmail.com if you are interested in developing the hacloco site and wiki or if you would like to plan an event</p>;
   </a>  
     }
