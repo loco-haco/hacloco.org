ee4cdc8a3f4276772f1f8f88cca83884a9d118d0
JavaG23 <geof.hoffman@gmail.com>
2024-05-20 18:46:22 -0400

    about page filler content

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,42 @@
 export default function Page() {
-    return <a
-    href="/"  
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:dark:bg-neutral-800/30"
-          target=""
-          rel="noopener noreferrer"><p>About Page- coming soon</p></a>;
-  }
+    return (<a
+        href="/"
+        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:dark:bg-neutral-800/30"
+        target=""
+        rel="noopener noreferrer">
+        <h1>About Page- coming soon</h1>
+
+        <br></br>
+
+        <h2>---What is a hackerspace?</h2>
+        -------a digital and/or physical space, infrastructure and people who use it to learn about and hack the world and everything in it.
+        <br></br>
+        <br></br>
+        <h1>---What is hacking? </h1>
+        <p>------the art of learning as much as you can about a subject or system, and then using what you know, to gain functionality from that subject or system that even its creators may not have intended or  thought possible.</p>
+
+        <br></br>
+        <h2>---Is hacking legal? </h2>
+        <p>------This question is like asking "is withdrawing funds from an ATM legal? or "Is driving 100mph legal?" The answer depends on how you do it.
+            If you withdraw funds from an atm using a wrecking bar instead of an atm card, no, it is not legal.
+            If you drive 100 mph anywhere other than a race track, no, it's not legal. Likewise, if you use hacking tools and knowledge to harm others, you will be breaking laws.
+            We at hacloco.org, do not participate in or condone activities that are illegal or that could be construed as illegal.
+            If you intend to break any laws, you are not welcome or authorized to access this website or attend events hosted or sponsored by this organization or its members.</p>
+        <br></br>
+        <h2>---What is hacloco? </h2>
+        <p>------ Right now, hacLoCo (and hacLoco.org) is just an idea that became a website.</p>
+        <br></br>
+        <h2>---But why?</h2>
+        <p> ------the idea is to bring people together, first online and then with in-person events.
+            What it becomes is ultimately up to you and everyone else that likes this idea and loves learning new tech with like-minded hackers. Life is a coalition of the willing.
+            <br></br>
+            <br></br>
+            </p>
+            <h2>---What does that even mean?</h2>
+            -------------What one person can do is limited. What an idea that inspires others can do is limited only by the collective imagination, determination, and collaboration of those willing to participate. You decide your own level of involvement in project hackerspace 
+
+        <br></br>
+
+    </a>)
+}
+
