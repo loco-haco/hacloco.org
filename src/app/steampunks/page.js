7e1424ceea83afc16bfe26cad940ce8bc53beb88
JavaG23 <geof.hoffman@gmail.com>
2024-05-27 16:23:40 -0400

    adding filler content

--- src/app/steampunks/page.js
+++ src/app/steampunks/page.js
@@ -3,6 +3,6 @@ export default function Page() {
     href="/"  
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:dark:bg-neutral-800/30"
           
-          rel="noopener noreferrer"><p>"Steampunks homepage-coming soon"</p>
+          rel="noopener noreferrer"><p>"Steampunks homepage-coming soon" some more content</p>
   </a>  
     }
