2d48b4e45d50ffba3bfe9ada4678e08c127f6e82
JavaG23 <geof.hoffman@gmail.com>
2024-06-17 08:59:56 -0400

    fixed build issue with Auth0 not being found

--- src/app/api/shows/route.js
+++ src/app/api/shows/route.js
@@ -1,4 +1,4 @@
-import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
+//import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
 import { NextResponse } from 'next/server';
 
 export const GET = withApiAuthRequired(async function shows(req) {
