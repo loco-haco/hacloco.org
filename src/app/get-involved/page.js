6e490f3284f9f613e2770cf73b48439b256cb42d
JavaG23 <geof.hoffman@gmail.com>
2025-02-17 15:59:49 -0500

    update signin

--- src/app/get-involved/page.js
+++ src/app/get-involved/page.js
@@ -1,8 +1,35 @@
 export default function Page() {
-    return <a
-    href="/"  
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:dark:bg-neutral-800/30"
-          
-          rel="noopener noreferrer"><p>"Get Involved" Page- coming soon. Email me at geof.hoffman@gmail.com if you are interested in developing the hacloco site and wiki or if you would like to plan an event</p>;
-  </a>  
-    }
+  return <div>
+    <div>
+      <a href="/"
+        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:dark:bg-neutral-800/30"
+        rel="noopener noreferrer">
+        <p>
+          "Get Involved" Page- coming soon. Email me at hacloco.org@gmail.com if you are interested in developing the hacloco site and wiki or if you would like to plan an event.
+
+
+        </p>
+      </a>
+    </div>
+    <div>
+
+
+      <ul>Todo/ wishlist items</ul>
+      <li>Migrate oAuth authentication to keycloak</li>
+      <li>launch wiki.js app </li>
+      <li>Build messaging app ( probably ATProtocol )</li>
+      <li>launch PDS for bluesky </li>
+
+      <div>
+
+        <h3>
+          right now, this website is statically served from a "serverless" cloud provider. This is a list to track items being worked on or with plans for future builds.  If you are interested in working on a development team, or hosting mirrors or  Infinispan cache(keycloak) reach out by email or DM on bluesky @hacloco.org
+
+
+        </h3>
+
+      </div>
+
+    </div>
+  </div>
+}
---
+++ src/app/signin/[[...sign-in]]/page.jsx
@@ -0,0 +1,8 @@
+import { SignIn } from '@clerk/nextjs'
+
+export default function Page() {
+  return <div class="flex flex-row min-h-screen justify-center items-center">
+<SignIn />
+</div>
+  
+}
