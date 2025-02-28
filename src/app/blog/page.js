2c518d4f786ddff903162436142a2d399e5c022f
JavaG23 <geof.hoffman@gmail.com>
2025-02-28 08:32:27 -0500

    water your garden

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,14 +1,34 @@
 import "./blog.css";
 export default function Page() {
 
-    return<div>
-         <a
-    href="/"  
-    target=""
-          rel="noopener noreferrer"><h1>HacLoCo Blog</h1> <h4>(click for home)</h4></a>
-        <a href="/blog/posts/1">           <h2>Essential Skills for technology-based Work</h2> <h3>a list of opensource technologies and platforms everyone should be familiar with</h3>
+  return (
 
-          <h4>Whether you’re aiming to build an app, create a software tool, or dive into the world of internet-based creativity, mastering certain skills is key to success in these fast-evolving fields. From programming languages to containerization and design principles, there is no shortage of skills to learn and no shortage of free resources to learn from. In this blog post, we'll explore the essential skills that every aspiring developer or creator should have in their toolkit. An emphasis is placed on free and open source tools and platforms. However, some proprietary software solutions are listed that have a free tier and can be useful tools for learning.......</h4></a>
-        
-          </div>
-  }
+    <div>
+      <a
+        href="/"
+        target=""
+        rel="noopener noreferrer"><h1>HacLoCo Blog</h1> <h4>(click for home)</h4></a>
+
+
+      <a href="/blog/posts/1">
+        <h2>
+          Essential Skills for technology-based Work
+        </h2>
+        <h3>
+          a list of opensource technologies and platforms everyone should be familiar with
+        </h3>
+        <h4>Whether you’re aiming to build an app, create a software tool, or dive into the world of internet-based creativity, mastering certain skills is key to success in these fast-evolving fields. From programming languages to containerization and design principles, there is no shortage of skills to learn and no shortage of free resources to learn from. In this blog post, we'll explore the essential skills that every aspiring developer or creator should have in their toolkit. An emphasis is placed on free and open source tools and platforms. However, some proprietary software solutions are listed that have a free tier and can be useful tools for learning.......</h4></a>
+
+<br/>
+
+
+
+
+      <a href="/blog/posts/2">
+        <h2>Hacking: the digital kung fu</h2>
+        <h4>
+          In the movie The Matrix, Neo, the hacker turned "chosen one," learns kung fu by simply downloading the knowledge directly into his mind........
+        </h4>
+      </a>
+    </div>)
+}
---
+++ src/app/blog/posts/2/page.js
@@ -0,0 +1,21 @@
+import "../../../blog/blog.css";
+export default function page(){
+
+    return( <div>
+<a href="https://docs.google.com/document/d/e/2PACX-1vQEnK0LEGZPSnMRruI0sTpxWfokpBvQsEL6ilfgqXVEIVTEoAih5I03LSdBx05ONtqj9Tm-tTTE_2CA/pub">
+<h1> Hacking: the digital kung fu</h1>
+</a>
+
+<h4>
+In the movie The Matrix, Neo, the hacker turned "chosen one," learns kung fu by simply downloading the knowledge directly into his mind. In a matter of seconds, he goes from having no martial arts training to performing complex fighting moves with ease. This moment is iconic, yet it's also deeply ironic. Kung fu, as it was originally meant, refers not just to a specific martial art but to any discipline or practice that requires patience, energy, and time to master. The term "kung fu" literally translates to "skill achieved through hard work"—a process that involves dedication and perseverance over time.
+  
+</h4><br/><h4>  
+Neo’s ability to download kung fu directly into his mind is the antithesis of this fundamental principle. Instead of spending years honing his skills, he gains instant mastery. While this technological shortcut seems impressive, it highlights an irony: true mastery of any skill—whether it’s kung fu or coding—requires the journey of practice, mistakes, and learning through experience. Neo's hacking skills afford him the ability to download knowledge instantly, bypassing the very thing that kung fu and many other skills demand: time and effort.
+</h4><br/><h4> 
+One could argue that it’s Neo’s expertise in hacking—the very thing that enables him to manipulate the digital world—that makes this shortcut possible. His technical mastery allows him to break through barriers and learn at an impossible speed. In contrast, real-world mastery, like the mastery of kung fu or technical skills such as coding, cannot be downloaded; it’s built, drop by drop, through patience and perseverance. Neo's journey in The Matrix serves as a reminder that while technology can offer shortcuts, the real depth of learning comes from the time and effort we put into our craft.
+</h4><br/><h4> 
+
+Hack Challenge: Some of you have criticized and complained about the choice to use green phosphor text color for this blog, an homage to the monochrome monitors that many of us may have learned our first computer languages on. Right now, you probably think I'm going to challenge you to change it, but that wouldn't be very challenging. Instead I challenge you to realize the truth, there is no green text. Once you realize this you can begin to understand how easy it is to change it. Also, bending spoons isn't impossible. I do it practically every time I scoop ice cream at 3 a.m.
+</h4><br/>
+</div>)
+}
